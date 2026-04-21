207ac2c7f400a6e8afb15b507d6d479ed7b756a3
Himanshu1520 <103326595+Himanshu1520@users.noreply.github.com>
2026-04-21 22:52:50 +0530

    Add: sqrtx - submission-2

---
+++ Data Structures & Algorithms/sqrtx/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long low=1,high=x;
+        long ans=0;
+
+        while(low<=high){
+            long mid=(low+high)/2;
+            if(mid*mid==x){
+                return mid;
+            }
+
+            if(mid*mid>x){
+                
+                high=mid-1;
+                
+            }
+
+            else{
+                ans=mid;
+                low=mid+1;
+            }
+        }
+
+        return ans;
+    }
+};
