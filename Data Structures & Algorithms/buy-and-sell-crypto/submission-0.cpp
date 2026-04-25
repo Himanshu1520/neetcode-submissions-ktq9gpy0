abe2860a386e041a4b9856b78e8823b15e1c207c
Himanshu1520 <103326595+Himanshu1520@users.noreply.github.com>
2026-04-25 21:02:38 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini= INT_MAX;
+        int profit=0;
+
+        for(int i=0;i<prices.size();i++){
+            mini=min(mini,prices[i]);
+
+
+            int temp=prices[i]-mini;
+
+            profit=max(profit,temp);
+        }
+
+        return profit;
+
+    }
+};
